f89e5a47202559cf4039281aec0501dc9fb5cecb
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-17 09:44:45 -0300

    Desktop Icons
    - Add correct text color;
    - Add containers to align icons on the desktop;
    - Add a few icons for future implementation.

--- src/components/DesktopIcons/DesktopIcon/DesktopIcon.jsx
+++ src/components/DesktopIcons/DesktopIcon/DesktopIcon.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import './DesktopIcon.css';
 
-// TODO: Make iconText white - maybe use a span tag
-// TODO: Move CSS rules to a separate file
-
 function DesktopIcon({ iconImage, iconText }) {
 	return (
-		<button style={{ display: 'block' }}>
+		<button id="desktop-icon">
 			<img src={iconImage} />
-			{iconText}
+			<span id="desktop-icon-text"> {iconText} </span>
 		</button>
 	);
 }
--- src/components/DesktopIcons/DesktopIconsContainer.jsx
+++ src/components/DesktopIcons/DesktopIconsContainer.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import DesktopIcon from './DesktopIcon/DesktopIcon';
 
+import './DesktopIconsContainer.css';
 import myComputerIcn from '../../assets/Icons/my-computer-icon.png';
+import networkIcn from '../../assets/Icons/network-neighborhood-icon.png';
+import recycleBinIcn from '../../assets/Icons/recycle-bin-icon.png';
+import documentIcn from '../../assets/Icons/document-icon.png';
+import folderIcn from '../../assets/Icons/folder-icon.png';
 
 function DesktopIconsContainer() {
 	return (
-		<div className="icons-container">
-			<DesktopIcon iconImage={myComputerIcn} iconText="My Computer" />
+		<div id="icons-container">
+			<div id="icons-column">
+				<DesktopIcon iconImage={myComputerIcn} iconText="My Computer" />
+				<DesktopIcon iconImage={networkIcn} iconText="Network Neighborhood" />
+				<DesktopIcon iconImage={recycleBinIcn} iconText="Recycle Bin" />
+				<DesktopIcon iconImage={documentIcn} iconText="Resume" />
+				<DesktopIcon iconImage={folderIcn} iconText="Portfolio" />
+			</div>
 		</div>
 	);
 }
