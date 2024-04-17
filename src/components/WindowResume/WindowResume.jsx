ef2fc4886dba117573ac98d7d42eb3b2b1af755d
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-17 13:52:00 -0300

    Add draggable functionality to WindowResume

--- src/components/WindowResume/WindowResume.jsx
+++ src/components/WindowResume/WindowResume.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import {} from 'react95';
+import React, { useRef } from 'react';
+import Moveable from 'react-moveable';
 
 import { Resume } from '../../util/Resume';
 import WindowBase from '../WindowBase/WindowBase';
 import ResumeTextField from './ResumeTextField';
 
 function WindowResume() {
+	const targetRef = useRef();
+
 	return (
-		<WindowBase
-			height="644"
-			width="600"
-			windowTitle="Resume - Notepad"
-			toolbarButtonsArr={['File', 'Edit', 'Search', 'Help']}
-		>
-			<ResumeTextField text={Resume} />
-		</WindowBase>
+		<div className="target" ref={targetRef}>
+			<WindowBase
+				resizable
+				height="644"
+				width="600"
+				windowTitle="Resume - Notepad"
+				toolbarButtonsArr={['File', 'Edit', 'Search', 'Help']}
+			>
+				<ResumeTextField text={Resume} />
+			</WindowBase>
+			<Moveable
+				target={targetRef}
+				draggable={true}
+				throttleDrag={1}
+				edgeDraggable={false}
+				startDragRotate={0}
+				throttleDragRotate={0}
+				zoom={0}
+				onDrag={(e) => {
+					e.target.style.transform = e.transform;
+				}}
+			/>
+		</div>
 	);
 }
 
