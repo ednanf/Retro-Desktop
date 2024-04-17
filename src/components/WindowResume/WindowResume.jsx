6e31159307cffcc9a99025b29d907dcc9a798e91
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-17 14:09:01 -0300

    Add TODOs

--- src/components/WindowResume/WindowResume.jsx
+++ src/components/WindowResume/WindowResume.jsx
@@ -5,20 +5,25 @@ import { Resume } from '../../util/Resume';
 import WindowBase from '../WindowBase/WindowBase';
 import ResumeTextField from './ResumeTextField';
 
+// FIXME: Make the text editable again
+// TODO: Make only the window draggable, not the textbox
+
 function WindowResume() {
 	const targetRef = useRef();
 
 	return (
-		<div className="target" ref={targetRef}>
-			<WindowBase
-				resizable
-				height="644"
-				width="600"
-				windowTitle="Resume - Notepad"
-				toolbarButtonsArr={['File', 'Edit', 'Search', 'Help']}
-			>
-				<ResumeTextField text={Resume} />
-			</WindowBase>
+		<>
+			<div className="target" ref={targetRef}>
+				<WindowBase
+					resizable
+					height="644"
+					width="600"
+					windowTitle="Resume - Notepad"
+					toolbarButtonsArr={['File', 'Edit', 'Search', 'Help']}
+				>
+					<ResumeTextField text={Resume} />
+				</WindowBase>
+			</div>
 			<Moveable
 				target={targetRef}
 				draggable={true}
@@ -31,7 +36,7 @@ function WindowResume() {
 					e.target.style.transform = e.transform;
 				}}
 			/>
-		</div>
+		</>
 	);
 }
 
