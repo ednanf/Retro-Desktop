389b2c03a31163092b8913f0c4a235088a57dd35
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-17 13:12:31 -0300

    Add editing capabilities to ResumeTextField

--- src/components/WindowResume/ResumeTextField.jsx
+++ src/components/WindowResume/ResumeTextField.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput } from 'react95';
 
 function ResumeTextField({ text }) {
-	return <TextInput multiline rows={28} variant="default" value={text} />;
+	const [textField, setTextField] = useState(text);
+
+	function handleChange(e) {
+		setTextField(e.target.value);
+	}
+
+	return (
+		<TextInput
+			multiline
+			rows={28}
+			variant="default"
+			value={textField}
+			onChange={handleChange}
+		/>
+	);
 }
 
 export default ResumeTextField;
