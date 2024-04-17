017b1f1d6c74bf554ceea1a028ebc628f41dba82
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-17 10:50:54 -0300

    Add active state to desktop icons

--- src/components/DesktopIcons/DesktopIcon/DesktopIcon.jsx
+++ src/components/DesktopIcons/DesktopIcon/DesktopIcon.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './DesktopIcon.css';
 
 function DesktopIcon({ iconImage, iconText }) {
+	const [selected, setSelected] = useState(false);
+
+	function handleClick() {
+		setSelected((prevState) => !prevState);
+	}
+
 	return (
-		<button id="desktop-icon">
+		<button id="desktop-icon" onClick={handleClick}>
 			<img src={iconImage} />
-			<span id="desktop-icon-text"> {iconText} </span>
+			<span id="desktop-icon-text" className={selected ? 'active' : ''}>
+				{iconText}
+			</span>
 		</button>
 	);
 }
