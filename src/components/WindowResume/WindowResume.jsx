e9fa9a60309edeb464a9315c938a6ef3632d9a3b
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-18 08:45:09 -0300

    Refactor WindowResume

--- src/components/WindowResume/WindowResume.jsx
+++ src/components/WindowResume/WindowResume.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { Resume } from '../../util/Resume';
-import WindowBase from '../WindowBase/WindowBase';
-import ResumeTextField from './ResumeTextField';
+import { Resume } from "../../util/Resume";
+import WindowBase from "../WindowBase/WindowBase";
+import ResumeTextField from "./ResumeTextField";
 
 function WindowResume() {
-	return (
-		<>
-			<div className="target">
-				<WindowBase
-					resizable
-					height="644"
-					width="600"
-					windowTitle="Resume - Notepad"
-					toolbarButtonsArr={['File', 'Edit', 'Search', 'Help']}
-				>
-					<ResumeTextField text={Resume} />
-				</WindowBase>
-			</div>
-		</>
-	);
+  return (
+    <WindowBase
+      resizable
+      height="644"
+      width="600"
+      windowTitle="Resume - Notepad"
+      toolbarButtonsArr={["File", "Edit", "Search", "Help"]}
+    >
+      <ResumeTextField text={Resume} />
+    </WindowBase>
+  );
 }
 
 export default WindowResume;
