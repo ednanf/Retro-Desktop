fb8bbea6a508e20d75632f23140c8ed9981a9513
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-18 19:16:09 -0300

    Create IE Toolbar

---
+++ src/components/WindowIE/IEToolbar/IEToolbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Toolbar, Button, Separator } from 'react95';
+
+import './IEToolbar.css';
+
+import openFdrIcn from '../../../assets/Icons/IE-Toolbar-Icons/open-folder-icon.png';
+import homeIcn from '../../../assets/Icons/IE-Toolbar-Icons/homepage-icon.png';
+
+function IEToolbar() {
+	return (
+		<>
+			<Separator />
+			<Toolbar id="ie-toolbar">
+				<Button variant="default" style={{ height: '32px', width: '32px' }}>
+					<img
+						src={openFdrIcn}
+						alt="Icon"
+						style={{ height: '22px', width: '22px' }}
+					/>
+				</Button>
+				<Button variant="default" style={{ height: '32px', width: '32px' }}>
+					<img
+						src={homeIcn}
+						alt="Icon"
+						style={{ height: '22px', width: '22px' }}
+					/>
+				</Button>
+			</Toolbar>
+			<Separator />
+		</>
+	);
+}
+
+export default IEToolbar;
