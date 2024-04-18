cf786ed29f26c58d2bc7695862e30e5176fb2a6e
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-18 10:59:11 -0300

    Remove moveable temporarily

--- src/components/WindowBase/WindowBase.jsx
+++ src/components/WindowBase/WindowBase.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import Moveable from 'react-moveable';
+// import Moveable from 'react-moveable';
 import { Window, WindowHeader, Button } from 'react95';
 
 import WindowToolbar from '../WindowToolbar/WindowToolbar';
@@ -9,6 +9,9 @@ import closeIcn from '../../assets/Icons/close-icon.png';
 
 // TODO: make toolbar be optional
 // To make it optional, make a boolean prop to render it only if true
+
+// FIXME: make the close button not be affected by the dragging target of moveable
+// right now, it's disables so I can close the window
 
 function WindowBase({
 	windowTitle,
@@ -38,7 +41,7 @@ function WindowBase({
 			</WindowHeader>
 			<WindowToolbar buttonsArray={toolbarButtonsArr} />
 			{children}
-			<Moveable
+			{/* <Moveable
 				target={targetRef}
 				dragTarget={dragTargetRef}
 				draggable={true}
@@ -50,7 +53,7 @@ function WindowBase({
 				onDrag={(e) => {
 					e.target.style.transform = e.transform;
 				}}
-			/>
+			/> */}
 		</Window>
 	);
 }
