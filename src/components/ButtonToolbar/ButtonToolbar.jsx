7558ad68ba290f579405009faa76a5ed12401ab1
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-21 17:59:34 -0300

    remove console log

--- src/components/ButtonToolbar/ButtonToolbar.jsx
+++ src/components/ButtonToolbar/ButtonToolbar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from 'react95';
 
 function ButtonToolbar({ icon }) {
-	console.log(icon.typeOf);
 	return (
 		<Button variant="default" style={{ height: '32px', width: '32px' }}>
 			<img src={icon} alt="" style={{ height: '16px', width: '16px' }} />
