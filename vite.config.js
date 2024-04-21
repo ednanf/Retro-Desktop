4f896d4de10ebfbb5771c9bf4478750af9c75927
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-20 22:41:18 -0300

    Add Million Linter to the project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 var plugins = [react()];
+var plugins = plugins;
+plugins.unshift(MillionLint.vite())
 export default defineConfig({
-	plugins: plugins,
+  plugins: plugins
 });
