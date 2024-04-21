30e2275d46793dff8eee4e71d546358f75bb7820
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-20 22:46:20 -0300

    Refactor TrayClock

--- src/components/StartMenuBar/TrayClock.jsx
+++ src/components/StartMenuBar/TrayClock.jsx
@@ -1,38 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tooltip } from 'react95';
 
 function TrayClock() {
-	let time = new Date().toLocaleTimeString([], {
-		hour: '2-digit',
-		minute: '2-digit',
-	});
+	const [currentTime, setCurrentTime] = useState(
+		new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+	);
 
-	// HACK: Has to be like this, otherwise it won't format like Windows95!
-	let dateDay = new Date().toLocaleDateString([], {
-		day: 'numeric',
-	});
+	const date = [
+		new Date().toLocaleDateString([], { day: 'numeric' }),
+		new Date().toLocaleDateString([], { month: 'long' }),
+		new Date().toLocaleDateString([], { year: 'numeric' }),
+	].join(' ');
 
-	let dateMonth = new Date().toLocaleDateString([], {
-		month: 'long',
-	});
-
-	let dateYear = new Date().toLocaleDateString([], {
-		year: 'numeric',
-	});
-
-	let date = `${dateDay} ${dateMonth} ${dateYear}`;
-
-	const [currentTime, setCurrentTime] = useState(time);
-
-	function updateTime() {
-		let time = new Date().toLocaleTimeString([], {
-			hour: '2-digit',
-			minute: '2-digit',
-		});
-		setCurrentTime(time);
-	}
-
-	setInterval(updateTime, 1000);
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCurrentTime(
+				new Date().toLocaleTimeString([], {
+					hour: '2-digit',
+					minute: '2-digit',
+				})
+			);
+		}, 1000);
+		return () => clearInterval(intervalId);
+	}, [setCurrentTime]);
 
 	return (
 		<Tooltip
