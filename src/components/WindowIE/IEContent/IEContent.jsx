1033123f625fac32222a2cd940a516628ae9f6b8
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-21 11:00:03 -0300

    Add social icons

--- src/components/WindowIE/IEContent/IEContent.jsx
+++ src/components/WindowIE/IEContent/IEContent.jsx
@@ -2,41 +2,47 @@ import React from 'react';
 import { Frame } from 'react95';
 
 import './IEContent.css';
+import linkedIn from '../../../assets/Content/MyComputer/linkedin-logo.png';
+import githubIcn from '../../../assets/Content/MyComputer/github-logo.png';
+import twitterIcn from '../../../assets/Content/MyComputer/twitter-logo.png';
 
 function IEContent() {
 	return (
 		<div id="ie-container">
 			<Frame variant="field" id="ie-content">
 				<h2>My Social Networks</h2>
+				<div className="divider"></div>
 				<div id="social-links">
-					<a
-						href="https://www.linkedin.com/in/ednanrff/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						LinkedIn
-					</a>
-					<a
-						href="https://github.com/ednanf"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						GitHub
-					</a>
-					<a
-						href="https://x.com/ednanf"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						X
-					</a>
-					<a
-						href="https://mastodon.net/@myusername"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Mastodon
-					</a>
+					<div className="link-container">
+						<img src={linkedIn} alt="LinkedIn" />
+						<a
+							href="https://www.linkedin.com/in/ednanrff/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							LinkedIn
+						</a>
+					</div>
+					<div className="link-container">
+						<img src={githubIcn} alt="GitHub" />
+						<a
+							href="https://github.com/ednanf"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							GitHub
+						</a>
+					</div>
+					<div className="link-container">
+						<img src={twitterIcn} alt="" />
+						<a
+							href="https://x.com/ednanf"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Twitter
+						</a>
+					</div>
 				</div>
 			</Frame>
 		</div>
