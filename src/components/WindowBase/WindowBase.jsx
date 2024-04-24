1a44321e3eab0d828d919d5c5fe06facd3399d28
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-04-24 13:38:51 -0300

    WindowBase
    - Add active/inactive windows
    - Change from onClick to onMouseDown for better reliability

--- src/components/WindowBase/WindowBase.jsx
+++ src/components/WindowBase/WindowBase.jsx
@@ -8,88 +8,92 @@ import WindowMenuBar from '../WindowMenuBar/WindowMenuBar';
 import './WindowBase.css';
 import closeIcn from '../../assets/Icons/UIIcons/close-icon.png';
 
-// TODO: make toolbar be optional
 // To make it optional, make a boolean prop to render it only if true
 
 // FIXME: Find a way to not make the screen scrollable if the window go out of bounds
 
 function WindowBase({
-	windowTitle,
-	toolbarButtonsArr = [],
-	height,
-	width,
-	resizable = false,
-	headerIcn,
-	closeWindow,
-	children,
+  windowTitle,
+  toolbarButtonsArr = [],
+  height,
+  width,
+  resizable = false,
+  headerIcn,
+  closeWindow,
+  children,
 }) {
-	// Sets the target component that will be moved - "target" prop
-	const targetRef = useRef();
+  // Sets the target component that will be moved - "target" prop
+  const targetRef = useRef();
 
-	// Defines the area that is draggable - "dragTarget" prop
-	const dragTargetRef = useRef();
+  // Defines the area that is draggable - "dragTarget" prop
+  const dragTargetRef = useRef();
 
-	// Window active management
-	const store = useStore();
-	const { globalZIndex, increaseGlobalZIndex } = store;
+  // Window active management
+  const store = useStore();
+  const { globalZIndex, increaseGlobalZIndex } = store;
 
-	const [zIndex, setZIndex] = useState(0);
+  const [zIndex, setZIndex] = useState(0);
 
-	function handleClick() {
-		setZIndex(globalZIndex + 1);
-		increaseGlobalZIndex();
-	}
+  function handleClick() {
+    setZIndex(globalZIndex + 1);
+    increaseGlobalZIndex();
+  }
 
-	console.log(globalZIndex, zIndex);
+  console.log(globalZIndex, zIndex);
 
-	return (
-		<Window
-			resizable={resizable}
-			shadow={true}
-			style={{
-				width: `${width}px`,
-				height: `${height}px`,
-				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)',
-				zIndex: zIndex,
-			}}
-			ref={targetRef}
-			onClick={handleClick}
-		>
-			<WindowHeader className="window-header">
-				<div className="draggable-area" ref={dragTargetRef}>
-					<div className="window-title non-selectable-text">
-						<img
-							src={headerIcn}
-							alt="Window header icon"
-							className="header-icon"
-						/>
-						<span>{windowTitle}</span>
-					</div>
-				</div>
-				<Button onClick={closeWindow}>
-					<img src={closeIcn} alt="Close icon" className="close-icon" />
-				</Button>
-			</WindowHeader>
-			<WindowMenuBar buttonsArray={toolbarButtonsArr} />
-			{children}
-			<Moveable
-				target={targetRef}
-				dragTarget={dragTargetRef}
-				draggable={true}
-				throttleDrag={1}
-				edgeDraggable={false}
-				startDragRotate={0}
-				throttleDragRotate={0}
-				zoom={0}
-				onDrag={(e) => {
-					e.target.style.transform = e.transform;
-				}}
-			/>
-		</Window>
-	);
+  return (
+    <Window
+      resizable={resizable}
+      shadow={true}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: zIndex,
+      }}
+      ref={targetRef}
+      onMouseDown={handleClick}
+    >
+      <WindowHeader
+        // FIXME: the first time 2 windows appear, both of them are marked as active
+        // FIXME: it's possible that every window spawns inactive after a few clicks
+        active={globalZIndex !== zIndex ? false : true}
+        className="window-header"
+      >
+        <div className="draggable-area" ref={dragTargetRef}>
+          <div className="window-title non-selectable-text">
+            <img
+              src={headerIcn}
+              alt="Window header icon"
+              className="header-icon"
+            />
+            <span>{windowTitle}</span>
+          </div>
+        </div>
+        <Button onClick={closeWindow}>
+          <img src={closeIcn} alt="Close icon" className="close-icon" />
+        </Button>
+      </WindowHeader>
+      <WindowMenuBar buttonsArray={toolbarButtonsArr} />
+      {children}
+      <Moveable
+        target={targetRef}
+        dragTarget={dragTargetRef}
+        draggable={true}
+        throttleDrag={1}
+        edgeDraggable={false}
+        startDragRotate={0}
+        throttleDragRotate={0}
+        zoom={0}
+        onDrag={e => {
+          e.target.style.transform = e.transform;
+        }}
+      />
+    </Window>
+  );
 }
 
 export default WindowBase;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 var plugins = [react()];
-var plugins = plugins;
-plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: plugins
+  plugins: plugins,
 });
